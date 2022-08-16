3f22cb363a578c96e353e39980458c0ea8fb15ce
S3ck1 <seck.dv15@gmail.com>
2022-08-16 09:45:50 -0600

    backdrop properties bug fixed

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ setDefaultSettings(preview);
 pickr.on("change", (color) => {
   let colorRGBA = color.toRGBA().toString(2);
   preview.style["backgroundColor"] = colorRGBA;
-  document.querySelector(".codeline-backgroundColor").textContent = colorRGBA.substring(5, colorRGBA.length-1) 
+  document.querySelector(".codeline-backgroundColor").textContent =
+    colorRGBA.substring(5, colorRGBA.length - 1);
 });
+
+let blurValue = "blur(50px)";
+let contrastValue = "contrast(50%)";
+let saturateValue = "saturate(50%)";
+preview.style.backdropFilter = `${blurValue} ${contrastValue} ${saturateValue}`;
 
 //A listener for all sliders
 slideSettings.forEach((element) => {
@@ -19,22 +25,27 @@ slideSettings.forEach((element) => {
     let value = slider.value;
     switch (property) {
       case "blur":
-        preview.style.backdropFilter = `blur(${value}px)`;
+        setBackdropProperty(property, value);
         document.querySelector(".codeline-blur").textContent = `${value}px`;
+        console.log(preview.style.backdropFilter);
         break;
-        case "contrast":
-        preview.style.backdropFilter = `contrast(${value}%)`;
+      case "contrast":
+        setBackdropProperty(property, value);
         document.querySelector(".codeline-contrast").textContent = `${value}%`;
+        console.log(preview.style.backdropFilter);
         break;
-        case "saturate":
-        preview.style.backdropFilter = `saturate(${value}%)`;
+      case "saturate":
+        setBackdropProperty(property, value);
         document.querySelector(".codeline-saturate").textContent = `${value}%`;
+        console.log(preview.style.backdropFilter);
         break;
-        case "border-radius":
+      case "border-radius":
         preview.style.borderRadius = `${value}%`;
-        document.querySelector(".codeline-borderRadius").textContent = `${value}%`;
+        document.querySelector(
+          ".codeline-borderRadius"
+        ).textContent = `${value}%`;
         break;
-        case "size":
+      case "size":
         preview.style.width = `${value}px`;
         preview.style.height = `${value}px`;
         document.querySelector(".codeline-width").textContent = `${value}px`;
@@ -50,13 +61,23 @@ slideSettings.forEach((element) => {
 
 function setDefaultSettings(container) {
   container.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  container.style.backdropFilter = "blur(50px)";
-  container.style.backdropFilter = "contrast(50%)";
-  container.style.backdropFilter = "saturate(50%)";
+  preview.style.backdropFilter = `blur(50px) contrast(50%) saturate(50%)`;
   container.style.borderRadius = "25%";
   container.style.width = "300px";
   container.style.height = "300px";
 }
-console.log(preview.style);
 
-console.log(document.getElementById("code").textContent);
+function setBackdropProperty(property, value) {
+  switch (property) {
+    case "blur":
+      blurValue = `blur(${value}px)`;
+      break;
+    case "contrast":
+      contrastValue = `contrast(${value}%)`;
+      break;
+    case "saturate":
+      saturateValue = `saturate(${value}%)`;
+      break;
+  }
+  preview.style.backdropFilter = `${blurValue} ${contrastValue} ${saturateValue}`;
+}
